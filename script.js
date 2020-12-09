54ed181cf7f2805b19c82abbb1d1911b7bafd4b3
AntGNgo <anthony.g.ngo@gmail.com>
2020-12-09 12:43:23 -0800

    Mobile cards and slide in animations

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const burger = document.querySelector(".burger")
 const nav = document.querySelector('.navigation')
+const cards = document.querySelectorAll('.card')
 
 
 const toggleNav = () => {
@@ -8,4 +9,18 @@ const toggleNav = () => {
         nav.classList.toggle('active')
     })
 }
+
+const delayCards = () => {
+    cards.forEach((card, i) => {
+        console.log('add')
+        // if(card.style.animation) {
+        //     card.style.animation = ''
+        // } else {
+            card.style.animation = `fadeInLeft 1.5s backwards ${i / 10 + .5}s`
+        // }
+    })
+}
+
 toggleNav()
+
+cards ? delayCards() : null
